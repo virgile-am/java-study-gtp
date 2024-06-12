95e161c3e96baadf46a6ace7d287cc4044327782
virgile-am <ndayambaje.virgile@amalitechtraining.org>
2024-06-12 12:39:50 +0200

    array

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+      String [] array ={"hello","Virgile","Set","Java","IDE"};
+      for (int i = 0; i < array.length; i++) {
+          System.out.println(array[i]);
+      }
+
+    }
+}
